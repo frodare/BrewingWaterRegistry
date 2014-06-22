4cf9db6194c2f684cf7ff1826805fcf5c038b2e7
Charles Howard <frodare@gmail.com>
2014-06-22 19:24:18 -0400

    got water profile working as a directive

--- app.js
+++ app.js
@@ -1,46 +1,186 @@
 var app = angular.module("brewingWaterRegistry", ['google-maps']);
 
+app.directive('waterEntryForm', function() {
+	return {
+		restrict: 'E',
+		replace: true,
+		templateUrl: 'waterEntryForm.html',
+		scope: {
+			'water': '=water'
+		}
+	};
+});
+
+app.directive('ionFormField', function() {
+	return {
+		restrict: 'E',
+		replace: true,
+		templateUrl: 'ionFormField.html',
+		scope: {
+			'ion': '=',
+			'ionName': '@',
+			'warn': '@'
+		},
+		controller: function ($scope, $element) {
+
+			var ion, ionNum;
+
+			function reset() {
+				ion = $scope.ion;
+				ionNum = +$scope.ion;
+				$element.removeClass('has-error').removeClass('has-warning').removeClass('has-success');
+				$scope.glyphicon = '';
+			}
+
+			function isEmpty() {
+				return ion === '' || ion == null;
+			}
+
+			function isValid() {
+				return (ionNum || ionNum === 0) && ionNum < 900;
+			}
+
+			function isWarn() {
+				return $scope.warn && ionNum > $scope.warn;
+			}
+
+			function setWarn() {
+				$scope.glyphicon = 'warning-sign';
+				$element.addClass('has-warning');
+			}
+
+			function setSuccess() {
+				$scope.glyphicon = 'ok';
+				$element.addClass('has-success');
+			}
+
+			function setError() {
+				$scope.glyphicon = 'remove';
+				$element.addClass('has-error');
+			}
+
+			function check() {
+				reset();
+
+				if(isEmpty()){
+					return;
+				}
+
+				if(!isValid()){
+					setError();
+					return;
+				}
+
+				if(isWarn()){
+					setWarn();
+				}else{
+					setSuccess();
+				}
+			}
+
+			$scope.$watch('ion', check, true);
+
+			check();
+		}
+	};
+});
+
+app.directive('raResult', function () {
+	return {
+		restrict: 'E',
+		replace: true,
+		templateUrl: 'raWaterFormField.html'
+	};
+});
+
+app.controller('raWaterController', function ($scope) {
+	
+	function toRa() {
+		var profile = $scope.water.profile;
+		if(!profile){
+			return 0;
+		}
+		var r = (profile.HCO * 0.819672131) - (0.714 * profile.Ca) - (0.585 * profile.Mg);
+		if(!r || isNaN(r)){
+			r = 0;
+		}
+		return r;
+	};
+
+	$scope.$watch('water.profile', function(){
+		$scope.water.ra = toRa();
+	}, true);
+	
+});
+
 
 app.controller('brewingWaterRegistryController', function ($scope) {
+
 	$scope.map = {
-		center: {latitude:30.4211323,longitude:-84.2517166},
-		draggable: true,
-		zoom: 6
+		zoom: 6,
+		center: {latitude:30.4211323,longitude:-84.2517166}
 	};
+
+	/*
+
+
+Calcium (ppm):  294  352
+Magnesium (ppm): 	 24
+
+
+Sulfates (ppm): 	 820
+Chloride (ppm): 	 36
+
+
+Sodium (ppm): 	 24 	44
+
+Carbonates (ppm): 	200  320
+
+*/
+	$scope.openEntry = function (){
+		console.log('openEntry: ', arguments);
+	};
+
+
+	$scope.water1 = {
+		profile: {
+			Ca: 42,
+			Mg: 12,
+			HCO: 142
+		}
+	};
+
+	$scope.water1.coords = {};
+
+
 
 	var lat = 31.4406;
 	var id = 1;
 
 
-	var entries = [
-		{
-			id: id++, 
-			latitude:31.4406, 
-			longitude:-82.2843, 
-			title: 'test'
-		}
-	];
+	var entries = [];
 
 	$scope.entries = entries;
 
 	$scope.addEntry = function () {
-		lat += 0.25;
-		entries.push({
-			id: id++, 
-			latitude:lat, 
-			longitude:-82.2843
-		});
+		console.log('addEntry(): ', $scope.water1, entries);
+		var entry = angular.copy($scope.water1);
+		entry.id = id++;
+		entry.icon = "http://maps.google.com/mapfiles/marker_white.png";
+		entries.push(entry);
 	};
+
+	entries.icon = "http://maps.google.com/mapfiles/marker_white.png";
 
 
 	$scope.map.events = {
 		click: function (map, name, args) {
 			var loc = args[0].latLng;
-			$scope.entries.push({
-				id: id++, 
+			
+			$scope.water1.coords = {
 				latitude: loc.lat(), 
 				longitude: loc.lng() 
-			});
+			};
 
 			$scope.$apply(function () {
 				$scope.mapInstance = map;			
